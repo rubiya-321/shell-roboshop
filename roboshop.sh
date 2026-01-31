48fcdb2b3f95c99bdcd487f3e75cf8609a51a0a5
Rubi <pathanrubiya269@gmail.com>
2026-01-31 23:14:17 +0530

    changes made

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,18 @@ do
     --query 'Instances[0].InstanceId' \
     --output text )
 
-
-    
+ if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
 done
