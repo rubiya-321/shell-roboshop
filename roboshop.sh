1622f8e69c17a70f748b19fda1ac0bf922c0fc11
Rubi <pathanrubiya269@gmail.com>
2026-01-31 22:58:44 +0530

    changes made

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+SG_ID="sg-08215fd9bc2dcf991" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z05013202FKF0ZL12WAOP"
-DOMAIN_NAME="daws88s.online"
+ZONE_ID=""
+DOMAIN_NAME=""
 
 for instance in $@
 do
