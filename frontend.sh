ac9da1d15439a133edda762a5e17b5f3ce121cdd
Rubi <pathanrubiya269@gmail.com>
2026-02-04 22:33:16 +0530

    actions taken

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
@@ -5,9 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 SCRIPT_DIR=$PWD
-MONGODB_HOST="mongodb.rubiya88s.online"
+MONGODB_HOST=mongodb.rubiya88s.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -25,31 +26,27 @@ VALIDATE(){
     fi
 }
 
+dnf module disable nginx -y &>>$LOGS_FILE
+dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+dnf install nginx -y &>>$LOGS_FILE
+VALIDATE $? "Installing Nginx"
 
-dnf module disable nginx -y &>>LOGS_FILE
-dnf module enable nginx:1.24 -y &>>LOGS_FILE
-dnf install nginx -y &>>LOGS_FILE
-
-VALIDATE $? "installing nginix"
-
-systemctl enable nginx &>>LOGS_FILE
+systemctl enable nginx  &>>$LOGS_FILE
 systemctl start nginx 
-
-VALIDATE $? "enabling and starting nginix"
+VALIDATE $? "Enabled and started nginx"
 
 rm -rf /usr/share/nginx/html/* 
-VALIDATE $? "removing default content"
+VALIDATE $? "Remove default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>LOGS_FILE
-VALIDATE $? "downloading the code"
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>LOGS_FILE
-VALIDATE "unzipping the code"
+unzip /tmp/frontend.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and unzipped frontend"
+
+rm -rf /etc/nginx/nginx.conf
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "copying nginx file"
+VALIDATE $? "Copied our nginx conf file"
 
-systemctl restart nginx 
-VALIDATE $? "restart nginix"
-
+systemctl restart nginx
+VALIDATE $? "Restarted Nginx"
